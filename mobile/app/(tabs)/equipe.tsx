b11f6cf3747861d8efc2911edb5747b08a13bee5
gigifggbgv <giovanna.verissimo.bezerra@escola.pr.gov.br>
2025-10-10 12:26:17 -0300

    inicio da pag de equipe, add cards

--- mobile/app/(tabs)/equipe.tsx
+++ mobile/app/(tabs)/equipe.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
@@ -19,61 +19,63 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View style={styles.card}>
+        <Image
+          source={{ uri: '...' }} 
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Card title</Text>
+          <Text style={styles.cardText}>
+            Some quick example text to build on the card title and make up the bulk of the card’s content.
+          </Text>
+          <Button title="Go somewhere" onPress={() => alert('Button Pressed')} />
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Image
+          source={{ uri: '...' }} 
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Card title</Text>
+          <Text style={styles.cardText}>
+            Some quick example text to build on the card title and make up the bulk of the card’s content.
+          </Text>
+          <Button title="Go somewhere" onPress={() => alert('Button Pressed')} />
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Image
+          source={{ uri: '...' }} 
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Card title</Text>
+          <Text style={styles.cardText}>
+            Some quick example text to build on the card title and make up the bulk of the card’s content.
+          </Text>
+          <Button title="Go somewhere" onPress={() => alert('Button Pressed')} />
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Image
+          source={{ uri: '...' }} 
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Card title</Text>
+          <Text style={styles.cardText}>
+            Some quick example text to build on the card title and make up the bulk of the card’s content.
+          </Text>
+          <Button title="Go somewhere" onPress={() => alert('Button Pressed')} />
+        </View>
+      </View>
     </ParallaxScrollView>
   );
 }
@@ -94,5 +96,32 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  card: {
+    width: '40%',
+    marginVertical: 50,
+    borderRadius: 30,
+    backgroundColor: '#FF0090',
+    elevation: 3,
+    shadowColor: '#000',
+    alignSelf: 'center',
+  },
+  cardImage: {
+    width: '100%',
+    height: 150,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  cardBody: {
+    padding: 10,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardText: {
+    fontSize: 14,
+    color: '#fff',
+    marginVertical: 10,
   },
 });
